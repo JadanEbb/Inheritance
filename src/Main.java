8500be67a2cf544620a47a52f26e29ea00bdf2c0
thoubot <ebbj0908@students.bowiestate.edu>
2026-09-17 10:21:55 -0400

    https://github.com/JadanEbb/Inheritance.git

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         Person person1 = new Person();
-        Person person2 = new Person(72, 70, "Blue", "Barack Obama", 170);
 
-        System.out.println(person2.name);
+//        Person person2 = new Person(72, 70, "Blue", "Barack Obama", 170);
+//        System.out.println(person2.name);
+        Person person2 = new Person(72, 70);
+        System.out.println(person2.age);
 
-
-
-
-
-
-
-
-
-
-
-
+        Person Person3 = person2.Call_Private();
+        System.out.println();
     }
-
 }
--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,4 @@
+//Private and protected are not allowed
 public class Person {
     //Height, age, color, name, weight: instance variable
 
@@ -11,7 +12,8 @@ public class Person {
 
     double weight;
 
-    Person(){
+    //Constructors can have private, protected, or public modifiers
+    Person() {
         //Default constructor can be empty
         height = 48;
         age = 18;
@@ -20,8 +22,16 @@ public class Person {
         weight = 140;
 
     }
+
+    // Protected members are accessible within the same package.
+    protected Person(double height, int age) {
+        // Parameters are local variables to the constructor.
+        this.height = height;
+        this.age = age;
+    }
+
     //overloaded constructor
-    Person(double height, int age, String color, String name, double weight) {
+    private Person(double height, int age, String color, String name, double weight) {
         // Parameters are local variables to the constructor.
         this.height = height;
         this.age = age;
@@ -30,6 +40,11 @@ public class Person {
         this.weight = weight;
     }
 
+    //Access, default return type, method Name, Parentheses, braces
+    public Person Call_Private() {
+        Person person3 = new Person(72, 70, "Blue", "Barack Obama", 170);
+        return person3;
+    }
 
 
 }
