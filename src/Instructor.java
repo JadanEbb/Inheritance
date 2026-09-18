121ec43ba7081d3d868938ad62c9d4a26eca00d6
thoubot <ebbj0908@students.bowiestate.edu>
2026-09-17 21:41:02 -0400

    Lab Three done

--- src/Instructor.java
+++ src/Instructor.java
@@ -1,5 +1,13 @@
 public class Instructor extends Person {
 
+    int assignments;
 
+    public Instructor(String firstName, String lastName, int age, int assignments) {
+        this.name = firstName + " " + lastName;
+        this.age = age;
+        this.assignments = assignments;
+    }
 
+    Instructor() {
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
 public class Main {
-    public static void main(String[] args) {
-        Person person1 = new Person();
-
+    static void main(String[] args) {
+//        Person person1 = new Person();
 //        Person person2 = new Person(72, 70, "Blue", "Barack Obama", 170);
 //        System.out.println(person2.name);
-        Person person2 = new Person(72, 70);
-        System.out.println(person2.age);
+//
+//        Person person2 = new Person(72, 70);
+//        System.out.println(person2.age);
+//
+//        Person Person3 = person2.Call_Private();
+//
+//        Student st1 = new Student();
+//        st1.age = 20;
+//        st1.gpa = 4.00;
+//        System.out.println(st1.gpa);
 
-        Person Person3 = person2.Call_Private();
+        Instructor instructor1 = new Instructor("Avijoy", "Chakma", 28, 4);
 
-        Student st1 = new Student();
-        st1.age = 20;
-        st1.gpa = 4.00;
-        System.out.println(st1.gpa);
+        System.out.println(instructor1.name);
+        System.out.println(instructor1.age);
+        System.out.println(instructor1.assignments);
     }
 }
